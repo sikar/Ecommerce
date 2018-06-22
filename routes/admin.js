5d83a3c5462049f6d1c0fcf8905e5b40b416788d
Shibaji Debnath <imshibaji@gmail.com>
2018-06-22 09:48:12 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next)=> {
-    res.send('Admin resource area.');
+    res.render('admin/index', { title: 'Admin Area.' });
 });
 
 module.exports = router;
