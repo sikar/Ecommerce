c2b489f25c060fa68fdb0856386a9c7c37efd431
Shibaji Debnath <imshibaji@gmail.com>
2018-06-30 10:04:45 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var PageController = require('../controller/admin/page');
 
 router.get('/', (req, res, next)=> {
     res.render('admin/index', { title: 'Admin Area.' });
+});
+
+router = PageController(router);
+
+
+router.get('/login', function(req, res, next){
+    res.render('admin/login', {title: 'Login Area'});
+});
+
+router.post('/login', function (req, res, next) {
+    var data = req.body;
+    res.json(data);
 });
 
 module.exports = router;
